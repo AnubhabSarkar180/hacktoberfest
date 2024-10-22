bcd6aad257f048c5dc26c5431069053088e38b1a
AnubhabSarkar180 <anubhabsarkar023@gmail.com>
2024-10-22 20:41:19 +0530

    Hello World in Java

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ LANGUAGE:JAVA
+ AUTHOR:Anubhab Sarkar
+ GITHUB:https://github.com/AnubhabSarkar180
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
